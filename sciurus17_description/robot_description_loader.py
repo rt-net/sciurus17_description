ff5b3e85a806e1334786b3283e48b4eebd606a36
Kuwagata <kuwagata@rt-net.jp>
2023-09-29 19:10:51 +0900

    robot_description_loader.py追加

---
+++ sciurus17_description/robot_description_loader.py
@@ -0,0 +1,21 @@
+# Copyright 2023 RT Corporation
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command
+
+
+class RobotDescriptionLoader():
+
+    def __init__(self):
+        self.robot_description_path = os.path.join(
+            get_package_share_directory('sciurus17_description'),
+            'urdf',
+            'sciurus17.urdf.xacro')
+
+    def load(self):
+        return Command([
+                'xacro ',
+                self.robot_description_path
+                ])
